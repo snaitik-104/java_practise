507591a7b3c6ee43eb286d1904402891e7fbf118
Naitik Sharma <snaitik104@gmail.com>
2022-12-09 11:46:13 +0530

    updated

---
+++ src/assignments/practise_8.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class practise_8 {
+
+        public static void main(String[] args)
+        {
+            String s="null";
+            if(s.length()==0)
+                System.out.println("zero");
+            else if(s==null)
+                System.out.println("null");
+            else
+                System.out.println("some");
+        }
+    }
+
+
